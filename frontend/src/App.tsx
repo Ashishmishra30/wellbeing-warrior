5f999230ddcc5eafcb154586c33bf9e82e59baa4
Vikas23sharma <112754659+Vikas23sharma@users.noreply.github.com>
2023-05-13 12:56:33 +0530

    login signup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './Pages/Home';
+import Login from './Pages/Login'; 
+import Signup from './Pages/Signup';
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      {/* <Login/> */}
+      {/* <Signup/> */}
     </div>
   );
 }
+
 
 export default App;
